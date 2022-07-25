49daa03cad1030add3c88f31a0873c8bcfd04c12
Shama8nchez <shamanouski@gmail.com>
2022-07-25 17:23:17 +0300

    feat: add solution_4

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -78,8 +78,15 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const time = endDate - startDate;
+  const nul = (num) => (num > 9 ? num : `0${num}`);
+  const msnul = (num) => (num > 9 ? num : `00${num}`);
+  const ms = time % 1000;
+  const sec = Math.floor((time / 1000) % 60);
+  const min = Math.floor((time / 1000 / 60) % 60);
+  const hour = Math.floor((time / 1000 / 60 / 60) % 24);
+  return `${nul(hour)}:${nul(min)}:${nul(sec)}.${msnul(ms)}`;
 }
 
 
@@ -99,8 +106,11 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const min = Math.floor((date / 1000 / 60) % 60);
+  const hour = Math.floor((date / 1000 / 60 / 60) % 12);
+  const angle = 0.5 * (60 * hour - 11 * min);
+  return Math.abs((angle > 180 ? 360 - angle : angle)) * (Math.PI / 180);
 }
 
 
